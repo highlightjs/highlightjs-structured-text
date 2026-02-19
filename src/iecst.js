ec84f85978ed8d5fc38c530853cf6fb61e7a50f7
Serhioromano <Serhioromano@outlook.com>
2026-02-19 12:57:35 +0300

    new version

--- src/iecst.js
+++ src/iecst.js
@@ -18,23 +18,35 @@ module.exports = function (hljs) {
         name: "Structured Text",
         case_insensitive: true,
         keywords: {
-            keyword:
-                "if then end_if elsif else case of end_case " +
-                "to do by while repeat end_while end_repeat for end_for from " +
-                "public private protected " +
-                "or and not xor le ge eq ne ge lt constant return exit at retain non_retain task with until using extend",
-            title:
+            tag:
                 "program end_program function end_function function_block end_function_block configuration " +
                 "end_configuration action end_action transition end_transition type end_type struct end_struct step " +
-                "end_step initial_step namespace end_namespace channel end_channel library end_library folder end_folder resource end_resource " +
-                "var var_global end_var var_input VAR_EXTERNAL var_out var_output var_in_out var_temp var_interval var_access var_config method end_method property end_property interface end_interface",
-            literal: "false true null ",
+                "end_step initial_step namespace end_namespace channel end_channel library end_library folder end_folder resource end_resource method end_method property end_property interface end_interface",
+            keyword: "if then end_if elsif else case of end_case to do by " +
+            "while repeat end_while end_repeat for end_for from public " +
+            "private protected constant return exit at retain non_retain " +
+            "persistent task with until using extend INTERVAL",
+            title:
+                "var var_global end_var var_input VAR_EXTERNAL var_out var_output var_in_out var_temp var_interval var_access var_config",
+            literal: "false true null",
             built_in:
                 "array pointer int sint dint lint usint uint udint ulint real lreal time date time_of_day date_and_time dt tod wstring string bool byte word dword lword ref_to any_num any_int any_string",
             function:
                 "mod abs acos asin atan cos exp expt ln log sin sqrt tan sel max min limit mux shl shr rol ror indexof sizeof adr ref adrinst bitadr add mul div sub trunc move",
         },
         contains: [
+            {
+                className: 'comment',
+                begin: '\\(\\*',
+                end: '\\*\\)',
+                relevance: 10
+            },
+            {
+                className: 'comment',
+                begin: '\\/\\*',
+                end: '\\*\\/',
+            },
+            hljs.COMMENT("//", "$"),
             {
                 className: "string",
                 begin: "'",
@@ -60,16 +72,51 @@ module.exports = function (hljs) {
                 begin: "TO_(BOOL|BYTE|D?L?WORD|L?TIME|DATE|DT|TOD|W?CHAR|W?STRING|U?S?D?L?INT|L?REAL)"
             },
             {
-                className: "symbol",
+                className: "function",
+                begin: "\\b[a-zA-Z_][a-zA-Z0-9_]*(?=\\()",
+            },
+            {
+                className: "operator",
+                begin: ":=|<>|<=|>=|=>|\\*\\*|[+\\-*/^<>=;]",
+            },
+            {
+                className: "operator",
+                begin: "\\b(NOT|AND|OR|XOR|AND_THEN|OR_THEN|MOD|LE|GE|EQ|NE|LT|GT)\\b",
+            },
+            {
+                className: "property",
+                begin: "(?<=\\b[a-zA-Z0-9_]+\\.)[a-zA-Z]+[a-zA-Z0-9_]*\\b",
+            },
+            {
+                className: "punctuation",
+                begin: "\\(|\\)|,|\\.",
+            },
+            {
+                className: "literal",
                 begin: "(D|T|DT|TOD)#[0-9:\\-_shmyd]*",
             },
             {
-                className: "symbol",
-                begin: "[a-zA-Z_]+#[a-zA-Z]*",
+                // GX Works2 Symbolic Addresses
+                className: "literal",
+                begin: "\\b(D|R|M)[0-9]{1,4}",
+            },
+            {
+                // GX Works2 Symbolic Addresses
+                className: "literal",
+                begin: "\\b(X|Y|TC|TS|TN|CC|CS|CN)[0-9]{1,3}",
             },
             {
                 className: "symbol",
-                begin: "[A-Za-z]{1,6}#[0-9_\\.e\\-]*",
+                begin: "[a-zA-Z_]+#[a-zA-Z]+",
+            },
+            {
+                className: "number",
+                begin: "[A-Za-z]{1,6}#[0-9_\\.e\\-]+",
+                relevance: 0,
+            },
+            {
+                className: "number",
+                begin: "16#[0-9_absdefABCDEF]*",
                 relevance: 0,
             },
             {
@@ -83,17 +130,16 @@ module.exports = function (hljs) {
                 relevance: 0,
             },
             {
-                className: "symbol",
+                className: "literal",
                 begin: "%(I|Q|M)(X|B|W|D|L)[0-9\\.]*",
             },
             {
-                className: "symbol",
+                className: "literal",
                 begin: "%(I|Q|M)[0-9.]*",
             },
             hljs.C_NUMBER_MODE,
-            hljs.COMMENT("//", "$"),
             hljs.C_BLOCK_COMMENT_MODE,
-            hljs.COMMENT("\\(\\*", "\\*\\)"),
+            hljs.C_LINE_COMMENT_MODE,
         ],
     };
 };
